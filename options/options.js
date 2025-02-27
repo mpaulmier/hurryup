f11fc862fc00959d7bd59a0395d06d01dd4ee837
Matthias Paulmier <hello@mattplm.fr>
2025-02-27 22:43:16 +0100

    Show preview of the player in the options

--- options/options.js
+++ options/options.js
@@ -2,6 +2,7 @@ document.addEventListener('DOMContentLoaded', () => {
     const showCurrentTimeCheckbox = document.getElementById('show-current-time-option')
     const showRemainingTimeCheckbox = document.getElementById('show-remaining-time-option')
     const showRateCheckbox = document.getElementById('show-rate-option')
+    const timeContainer = document.getElementById('time-container')
 
     browser.storage.local.get(['showCurrentTime', 'showRemainingTime', 'showRate'], (result) => {
         showCurrentTimeCheckbox.checked = !!result.showCurrentTime
@@ -25,7 +26,40 @@ document.addEventListener('DOMContentLoaded', () => {
             })
         })
     }
-    showCurrentTimeCheckbox.addEventListener('change', saveOptions)
-    showRemainingTimeCheckbox.addEventListener('change', saveOptions)
-    showRateCheckbox.addEventListener('change', saveOptions)
+
+    const updatePreview = () => {
+        const oldContainer = document.getElementById('demo-container')
+        if (!!oldContainer) oldContainer.remove()
+        const timeContainer = document.getElementById('time-container')
+        const newElement = document.createElement('span')
+        newElement.id = 'demo-container'
+        newElement.append(' (')
+        if (showRateCheckbox.checked) {
+            newElement.append('x')
+            newElement.append('1.5')
+            newElement.append(' → ')
+        }
+        if (showCurrentTimeCheckbox.checked) {
+            newElement.append('32:53')
+            newElement.append(' / ')
+        }
+        if (showRemainingTimeCheckbox.checked) {
+            newElement.append("45:39")
+        } else {
+            newElement.append('1:18:32')
+        }
+        newElement.append(')')
+        timeContainer.appendChild(newElement)
+    }
+
+    const saveAndUpdate = () => {
+        saveOptions()
+        updatePreview()
+    }
+
+    updatePreview()
+
+    showCurrentTimeCheckbox.addEventListener('change', saveAndUpdate)
+    showRemainingTimeCheckbox.addEventListener('change', saveAndUpdate)
+    showRateCheckbox.addEventListener('change', saveAndUpdate)
 })
